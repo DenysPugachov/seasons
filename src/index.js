f6fca1843819ce3fc54aed43d49d2ca7dc880b7f
Denys <pugachovdenys@gmail.com>
2021-01-24 01:50:38 +0200

    refactor with renderContent()

--- src/index.js
+++ src/index.js
@@ -23,16 +23,24 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
+      return <div>Error: { this.state.errorMessage }</div>;
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} lon={this.state.lon} />;
+      return <SeasonDisplay lat={ this.state.lat } lon={ this.state.lon } />;
     }
 
     return <Spinner message="Please accept location request..." />;
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderContent() }
+      </div>
+    );
   }
 }
 
