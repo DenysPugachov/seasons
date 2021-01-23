1b076aec99b9b3f9d6f4cb84a59a1b3a352fdc04
Denys <pugachovdenys@gmail.com>
2021-01-24 01:59:17 +0200

    Updates

---
+++ precache-manifest.45dc181588a9d2f3144ed3b1b82ee6b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f73e6654ee748b0f3545993c4b9a3af5",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "fc90fb58bb51341b1a4d",
+    "url": "/seasons/static/css/main.b8aeca90.chunk.css"
+  },
+  {
+    "revision": "c2d246ecc2b83ccac6a6",
+    "url": "/seasons/static/js/2.8b77c9df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/seasons/static/js/2.8b77c9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc90fb58bb51341b1a4d",
+    "url": "/seasons/static/js/main.13ed0b96.chunk.js"
+  },
+  {
+    "revision": "7aa38b62a80534558f86",
+    "url": "/seasons/static/js/runtime-main.53d2d0be.js"
+  }
+]);
